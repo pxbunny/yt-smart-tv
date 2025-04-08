c7b72fd30c697e4d3c2480bebaf66658d42190d6
dae-ne <rafalcz29@gmail.com>
2025-04-09 01:09:36 +0200

    Update the tv icon

--- src/components/io-tv-outline-icon.svelte
+++ src/components/io-tv-outline-icon.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height={height} width={width}>
-  <rect x="32" y="96" width="448" height="272" rx="32.14" ry="32.14" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/>
+  <rect x="43" y="96" width="426" height="272" rx="32.14" ry="32.14" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/>
   <path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M128 416h256"/>
 </svg>
