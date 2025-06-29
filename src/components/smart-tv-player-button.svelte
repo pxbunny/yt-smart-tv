d876274250eeed2bbdf97d1a19cc0c635c9bf200
dae-ne <rafalcz29@gmail.com>
2025-06-29 22:56:29 +0200

    Add icon to the smart tv player button

--- src/components/smart-tv-player-button.svelte
+++ src/components/smart-tv-player-button.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
+  import IoTvOutlineIcon from './io-tv-outline-icon.svelte';
+
   export let id: string | undefined = undefined;
   export let onClick: (() => void) | undefined = undefined;
 </script>
 
-<button id={id} on:click={onClick}>
-  TV
+<button
+  id={id}
+  class="ytp-button"
+  on:click={onClick}
+  data-tooltip-target-id="smart-tv-button"
+  data-title-no-tooltip="Smart TV"
+  aria-label="Smart TV"
+  title="Smart TV"
+>
+  <IoTvOutlineIcon width="24" height="24" />
 </button>
