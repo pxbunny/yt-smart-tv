c5672312387feb85a156e03586cdeb3d4b146d76
dae-ne <rafalcz29@gmail.com>
2025-10-03 19:41:54 +0200

    Update tv mode exit handling

--- src/content-script-tv.ts
+++ src/content-script-tv.ts
@@ -1,10 +1,31 @@
 import requests from 'requests';
 
-document.addEventListener(
-    'keydown',
-    event => {
-        if (event.key === 'Escape' || event.key === 'Backspace')
-            chrome.runtime.sendMessage(requests.CLOSE_SMART_TV);
-    },
-    true
-);
+const callback: MutationCallback = (mutationList: MutationRecord[], observer: MutationObserver) => {
+    mutationList.forEach(mutation => {
+        if (mutation.addedNodes.length < 1) return;
+
+        const query = `yt-formatted-string[idomkey="ytLrOverlayPanelHeaderRendererTitle"]`;
+        const exitHeader = Array.from(document.querySelectorAll(query)).find(
+            el => el.textContent === 'Exit YouTube'
+        );
+
+        if (!exitHeader) return;
+
+        observer.disconnect();
+        chrome.runtime.sendMessage(requests.CLOSE_SMART_TV);
+    });
+};
+
+const timeoutHandler = () => {
+    const exitScreenContainer = document.querySelector('zylon-provider-3');
+
+    if (!exitScreenContainer) {
+        setTimeout(timeoutHandler, timeoutDelayMs);
+        return;
+    }
+
+    new MutationObserver(callback).observe(exitScreenContainer, { childList: true });
+};
+
+const timeoutDelayMs = 200;
+setTimeout(timeoutHandler, timeoutDelayMs);
